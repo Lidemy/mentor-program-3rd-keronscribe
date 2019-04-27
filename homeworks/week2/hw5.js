c616238717bba7bb8d9ca1c6ad8d558b504cc0e7
Cain <Cain@chi.local>
2019-04-27 13:25:39 +0800

    完成homework3.4.5

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,26 @@
-function join(str, concatStr) {
-  return concatStr;
+function join(arr, concatStr) {
+  let i = 0;
+  let result = '';
+  while (i < arr.length) {
+    if (i !== arr.length - 1) {
+      result += arr[i] + concatStr;
+    } else if (i === arr.length - 1) {
+      result += arr[i];
+    }
+    i += 1;
+  }
+  return result;
 }
 
 function repeat(str, times) {
-  return times;
+  let i = 0;
+  let string = '';
+  while (i < times) {
+    string += str;
+    i += 1;
+  }
+  return string;
 }
 
-console.log(join('a', '!'));
-console.log(repeat('a', 5));
+console.log(join([1, 2, 3], '!'));
+console.log(repeat('a', 9));
