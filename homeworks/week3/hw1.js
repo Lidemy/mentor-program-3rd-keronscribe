be958ddaba05b3e3df9ccf3857af0a3c44cd0937
Cain <Cain@chi.local>
2019-05-27 12:47:44 +0800

    寫完作業一

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,5 +1,15 @@
 function stars(n) {
-  console.log(n);
+  let items = '';
+  let arr = 0;
+  arr = [];
+  for (let i = 0; i < n; i += 1) {
+    items += '*';
+    arr.push(items);
+  }
+  return arr;
 }
+
+console.log(stars(6));
+
 
 module.exports = stars;
